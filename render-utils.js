850d82143345ea6b50b8cbb5a9c351c9ada23e84
Denzel <dbartolaba123@gmail.com>
2022-01-12 14:32:40 -0800

    Appended contact in the DOM

--- render-utils.js
+++ render-utils.js
@@ -1,8 +1,15 @@
 export function renderParticipant(participant) {
-    const participantsEl = document.createElement('p');
+    const participantsEl = document.createElement('div');
+    const participantNameEl = document.createElement('p');
+    const participantContactEl = document.createElement('p');
 
     participantsEl.classList.add('person');
-    participantsEl.textContent = participant.name;
-
+    participantNameEl.classList.add('name-part');
+    participantContactEl.classList.add('contact-part');
+    
+    participantNameEl.textContent = participant.name;
+    participantContactEl.textContent = participant.contact;
+    
+    participantsEl.append(participantNameEl, participantContactEl);
     return participantsEl;
 }
