f38841de55beb66d9871bc0146b21009b4dc131a
Denzel <dbartolaba123@gmail.com>
2022-01-11 17:20:25 -0800

    Added in displayWorkshop function

--- workshops/workshops.js
+++ workshops/workshops.js
@@ -1,9 +1,33 @@
-import { checkAuth, logout } from '../fetch-utils.js';
+import { checkAuth, getWorkshops, logout } from '../fetch-utils.js';
 
 checkAuth();
+
+const participantEl = document.querySelector('.participant-list');
 
 const logoutButton = document.getElementById('logout');
 
 logoutButton.addEventListener('click', () => {
     logout();
+});
+
+function displayWorkshops(workshop) {
+    participantEl.textContent = '';
+
+    for (let person of workshop) {
+        const personEl = document.createElement('div');
+        const nameEl = document.createElement('h2');
+        const workshopEl = document.createElement('div');
+
+        workshopEl.classList.add('workshops');
+        personEl.classList.add('participant');
+
+        nameEl.textContent = person.name;
+    }
+}
+
+
+window.addEventListener('load', async() => {
+    const workshop = await getWorkshops();
+
+    displayWorkshops(workshop);
 });
