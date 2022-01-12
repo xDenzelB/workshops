5b9f2ed5bb6dc6b34453ae258e489ed815764e4d
Denzel <dbartolaba123@gmail.com>
2022-01-12 14:53:55 -0800

    Finished TDD

--- test/example.test.js
+++ test/example.test.js
@@ -1,18 +1,19 @@
 // IMPORT MODULES under test here:
 // import { example } from '../example.js';
+import { renderParticipant } from '../render-utils.js';
 
 const test = QUnit.test;
 
-test('time to test a function', (expect) => {
+test('Should get a DIV element', (expect) => {
     //Arrange
     // Set up your arguments and expectations
-    const expected = true;
+    const expected = '<p class="name-part"></p><p class="contact-part"></p>';
     
     //Act 
     // Call the function you're testing and set the result to a const
-    const actual = true;
+    const actual = renderParticipant('div');
 
     //Expect
     // Make assertions about what is expected versus the actual result
-    expect.equal(actual, expected);
+    expect.equal(actual.innerHTML, expected);
 });
